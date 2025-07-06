94c77582206bb4ef4b19072f908d4c6577b37576
ManojPinnamaneni <108618667+ManojPinnamaneni@users.noreply.github.com>
2025-07-07 00:59:00 +0530

    Time: 0 ms (100.00%) | Memory: 32 MB (72.42%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.cpp
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.cpp
@@ -1,20 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
-        int n=score.size();
-        int m=score[0].size();
-        for(int i=0;i<n;i++){
-            int maxi=INT_MIN,maxir=-1;
-            for(int j=i;j<n;j++){
-                if(score[j][k]>maxi){
-                    maxi=score[j][k];
-                    maxir=j;
-                }
-            }
-            for(int j=0;j<m;j++){
-                swap(score[i][j],score[maxir][j]);
-            }
-        }
+        sort(score.begin(), score.end(), [&](const vector<int>& a, const vector<int>& b) {
+            return a[k] > b[k];
+        });
         return score;
     }
 };
