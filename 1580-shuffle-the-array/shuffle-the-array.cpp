630c14e581355823cd22b6fd0c5121f3743baf89
ManojPinnamaneni <108618667+ManojPinnamaneni@users.noreply.github.com>
2025-07-07 22:40:48 +0530

    Time: 8 ms (17.12%) | Memory: 13.4 MB (82.81%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.cpp
+++ 1580-shuffle-the-array/shuffle-the-array.cpp
@@ -1,11 +1,33 @@
+// class Solution {
+// public:
+//     vector<int> shuffle(vector<int>& nums, int n) {
+//         vector<int> ans;
+//         for(int i=0;i<n;i++){
+//             ans.push_back(nums[i]);
+//             ans.push_back(nums[n+i]);
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> ans;
-        for(int i=0;i<n;i++){
-            ans.push_back(nums[i]);
-            ans.push_back(nums[n+i]);
-        }
+
+       int i = 0;
+       int j = n;
+
+       vector<int> ans;
+
+       while(i <= n && j < nums.size()){
+
+        ans.push_back(nums[i]);
+        ans.push_back(nums[j]);
+
+        i++;
+        j++;
+
+       }
+
         return ans;
     }
 };
