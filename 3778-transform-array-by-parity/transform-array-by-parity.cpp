c99c6e62530a4c55570f44ca8c4785fa8f95c07f
ManojPinnamaneni <108618667+ManojPinnamaneni@users.noreply.github.com>
2025-07-07 11:24:11 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (24.83%) - LeetSync

--- 3778-transform-array-by-parity/transform-array-by-parity.cpp
+++ 3778-transform-array-by-parity/transform-array-by-parity.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector<int> transformArray(vector<int>& nums) {
         int n=nums.size();
+        vector<int> v;
         for(int i=0;i<n;i++){
             if(nums[i]%2==0){
-                nums[i]=0;
+                v.insert(v.begin(),0);
             }
             else{
-                nums[i]=1;
+                v.push_back(1);
             }
         }
-        sort(nums.begin(),nums.end());      
-        return nums;  
+        return v;  
     }
 };
