953842fbd9792c51924a6b4888291eec6a7e6392
ManojPinnamaneni <108618667+ManojPinnamaneni@users.noreply.github.com>
2025-07-08 23:12:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (56.68%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int maxi=INT_MIN;
+        for(int i=0;i<accounts.size();i++){
+            int sum=accumulate(accounts[i].begin(),accounts[i].end(),0);
+            if(sum>maxi){
+                maxi=sum;
+            }
+        }
+        return maxi;
+    }
+};
