df20a1492266ece55cea9808f5705153e3455fad
ManojPinnamaneni <108618667+ManojPinnamaneni@users.noreply.github.com>
2025-07-11 00:52:34 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (31.76%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        if(nums.size()==1){
-            return 0;
-        }
-        for(int i=0;i<nums.size();i++){
-            if(i==0){
-                if(nums[i]>nums[i+1]){
-                    return i;
-                }
-            }else if (i==nums.size()-1){
-                if(nums[i-1]<nums[i]){
-                    return i;
-                }
+        int low = 1, high = nums.size() - 2;
+
+        // Edge cases
+        if (nums.size() == 1) return 0;
+        if (nums[0] > nums[1]) return 0;
+        if (nums[nums.size() - 1] > nums[nums.size() - 2]) return nums.size() - 1;
+
+        // Binary search
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if (nums[mid] > nums[mid - 1]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
-            else{
-                if(nums[i]>nums[i-1] and nums[i]>nums[i+1]){
-                    return i;
-                }
-            }
         }
-        return 0;
+        return -1; 
     }
 };
