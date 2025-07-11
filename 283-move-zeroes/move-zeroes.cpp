d7eecbb9eeee4cdaffd03b451eb2445ddcaf60ca
ManojPinnamaneni <108618667+ManojPinnamaneni@users.noreply.github.com>
2025-07-12 01:59:21 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (79.93%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i = 0;
-        int n = nums.size();
-        while (i < n) {
-            if (nums[i] == 0) {
-                nums.erase(nums.begin() + i);
-                nums.push_back(0);
-                n--; // size of the non-zero area reduces
-            } else {
-                i++;
+        int lastNonZero = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[lastNonZero++], nums[i]);
             }
         }
     }
